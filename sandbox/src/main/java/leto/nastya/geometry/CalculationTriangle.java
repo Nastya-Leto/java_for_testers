cb04d4cd263560cfc73a655b99e9c9ab5789ae88
nastya-leto <soidberg91@gmail.com>
2024-03-30 20:29:11 +0300

    Добавлены исключения и тесты на них

--- sandbox/src/main/java/leto/nastya/geometry/CalculationTriangle.java
+++ sandbox/src/main/java/leto/nastya/geometry/CalculationTriangle.java
@@ -7,9 +7,18 @@ public class CalculationTriangle {
     public double side3;
 
     public CalculationTriangle(double side1, double side2, double side3) {
+
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
+
+        if (side1 < 0 || side2 < 0 || side3 < 0) {
+            throw new IllegalArgumentException("Сторона треугольника не может быть отрицательной");
+        }
+
+        if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
+            throw new IllegalArgumentException("Cумма двух сторон треугольника не может быть меньше третьей стороны");
+        }
     }
 
     public double calculationAreaTriangle() {
@@ -18,9 +27,7 @@ public class CalculationTriangle {
         return Math.sqrt(p2);
     }
 
-    public double calculationPerimeter(){
-        return side1+side2+side3;
+    public double calculationPerimeter() {
+        return side1 + side2 + side3;
     }
-
-
 }
--- sandbox/src/test/java/leto/nastya/geometry/CalculationTriangleTests.java
+++ sandbox/src/test/java/leto/nastya/geometry/CalculationTriangleTests.java
@@ -3,8 +3,6 @@ package leto.nastya.geometry;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static java.lang.Float.NaN;
-
 public class CalculationTriangleTests {
 
     @Test
@@ -22,22 +20,74 @@ public class CalculationTriangleTests {
     }
 
     @Test
-    void calculateAreaWithZeroTest() {
-        var s = new CalculationTriangle(0, 8.0, 9.0);
-        double result = s.calculationAreaTriangle();
-        Assertions.assertEquals(NaN, result, "Некорректные входящие данные");
-    }
-
-    @Test
     void calculatePerimeterFirstPositiveTest() {
         var s = new CalculationTriangle(4, 7.5, 3.0);
         double result = s.calculationPerimeter();
         Assertions.assertEquals(14.5, result);
     }
+
     @Test
     void calculatePerimeterSecondPositiveTest() {
         var s = new CalculationTriangle(10.0, 8.0, 9.0);
         double result = s.calculationPerimeter();
         Assertions.assertEquals(27.0, result);
+    }
+
+    @Test
+    void cannotCreateWithTriangleNegativeSideA() {
+        try {
+            var s = new CalculationTriangle(-10.0, 8.0, 9.0);
+            Assertions.fail("Стороны не отрицательные");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест успешный");
+        }
+    }
+
+    @Test
+    void cannotCreateWithTriangleNegativeSideB() {
+        try {
+            var s = new CalculationTriangle(10.0, -8.0, 9.0);
+            Assertions.fail("Стороны не отрицательные");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест успешный");
+        }
+    }
+
+    @Test
+    void cannotCreateWithTriangleNegativeSideC() {
+        try {
+            var s = new CalculationTriangle(10.0, 8.0, -9.0);
+            Assertions.fail("Стороны не отрицательные");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест успешный, поймали исключение");
+        }
+    }
+
+    @Test
+    void cannotCreateWithTriangleIncorrectSideA() {
+        try {
+            var s = new CalculationTriangle(0, 8.0, 9.0);
+            Assertions.fail("Условие правильного треугольника выполняется");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест успешный, поймали исключение");
+        }
+    }
+    @Test
+    void cannotCreateWithTriangleIncorrectSideB() {
+        try {
+            var s = new CalculationTriangle(12, 1.0, 9.0);
+            Assertions.fail("Условие правильного треугольника выполняется");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест успешный, поймали исключение");
+        }
+    }
+    @Test
+    void cannotCreateWithTriangleIncorrectSideC() {
+        try {
+            var s = new CalculationTriangle(5, 7.0, 1.0);
+            Assertions.fail("Условие правильного треугольника выполняется");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест успешный, поймали исключение");
+        }
     }
 }
